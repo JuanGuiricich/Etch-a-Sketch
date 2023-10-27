0585fc1bb0994c20f880d3cd62d079e5fe9638eb
Juan Guiricich <jguiricich@gmail.com>
2023-10-27 18:02:17 -0300

    add resize functionallity

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const gridContainer = document.querySelector('#gridContainer');
-const columns = 27;
-const rows = 27;
+const columns = 16;
+const rows = 16;
+const clearButton = document.querySelector('#clearButton');
+const resizeButton = document.querySelector('#resizeButton');
+const resizeInput = document.querySelector('#resizeInput');
+
+
 
 function createGrid(rows, columns) {
   const totalCells = rows * columns;
   for (let i = 0; i < totalCells; i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('gridItem');
-    const cellSize = 100 / columns; // Adjust this as needed
+    const cellSize = 100 / columns;
     newDiv.style.width = cellSize + '%';
     newDiv.style.paddingBottom = cellSize + '%';
     gridContainer.appendChild(newDiv);
@@ -18,8 +23,41 @@ createGrid(rows, columns);
 
 const gridItems = document.querySelectorAll('.gridItem');
 
+
+clearButton.addEventListener('click', () => {
+  console.log('clear button clicked');
+  gridItems.forEach((gridItem) => {
+    gridItem.classList.remove('hovered');
+  });
+});
+
 gridItems.forEach((gridItem) => {
   gridItem.addEventListener('mouseover', () => {
     gridItem.classList.add('hovered');
+  });
+});
+
+resizeButton.addEventListener('click', () => {
+  console.log('resize button clicked');
+  const newRows = resizeInput.value;
+  const newColumns = resizeInput.value;
+  gridContainer.innerHTML = '';
+  if (newRows > 100 || newColumns > 100 && newRows < 1 || newColumns < 1) {
+    alert('Please enter a number between 1 and 100');
+    createGrid(rows, columns);
+  }else {
+    createGrid(newRows, newColumns);
+  }
+  const gridItems = document.querySelectorAll('.gridItem');
+  gridItems.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.classList.add('hovered');
+    });
+  });
+  clearButton.addEventListener('click', () => {
+    console.log('clear button clicked');
+    gridItems.forEach((gridItem) => {
+      gridItem.classList.remove('hovered');
+    });
   });
 });
